5661be8f2a0db9a2543486e0193386cc49cfe11f
neeasthana <neeasthana@gmail.com>
2022-02-12 14:04:37 -0500

    added method to follow other accounts

--- python/customer.py
+++ python/customer.py
@@ -16,8 +16,18 @@ class User:
 class CustomerProfile:
     def __init__(self, user: User, instagram_username = None):
         self.user = user
-        self.instagram = InstagramAccount(instagram_username)
+        self.instagram_profile = InstagramAccount(instagram_username)
+        self.follwing_accounts = []
 
 
-user = User("Kobe the GoldenDoodle")
-customer = CustomerProfile(user, "mindmatterswithdiv")
+    def follow_instagram_accounts(self, *instagram_handles):
+        for handle in instagram_handles:
+            account = InstagramAccount(handle)
+            self.follwing_accounts.append(account)
+
+
+
+if __name__ == "__main__":
+    user = User("Kobe the GoldenDoodle")
+    customer = CustomerProfile(user, "mindmatterswithdiv")
+    customer.follow_instagram_accounts("mindmatterswithdiv", "curly_therapist", "selfcare4yu")
