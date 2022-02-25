e925d8560a5969f815f0625dd5748e45c769c707
neeasthana <neeasthana@gmail.com>
2022-02-24 22:52:24 -0500

    download all image content to post on html page

--- python/generator.py
+++ python/generator.py
@@ -1,5 +1,8 @@
 from domonic.html import *
 from customer import User, CustomerProfile
+from accounts import InstagramPost
+import os
+import requests
 
 class HtmlGenerator:
     def __init__(self, profile: CustomerProfile):
@@ -33,6 +36,7 @@ class HtmlGenerator:
 class TopPostsCustomerProfileHtmlGenerator(HtmlGenerator):
     def __init__(self, profile: CustomerProfile):
         super().__init__(profile)
+        _create_cache_folder()
 
 
     def get_body(self):
@@ -41,10 +45,12 @@ class TopPostsCustomerProfileHtmlGenerator(HtmlGenerator):
         most_liked_posts = [(account, account.get_most_liked_post()) for account in self.profile.follwing_accounts]
 
         for account, post in most_liked_posts:
+            download_instagram_image(post)
+
             header = h2(account.username)
             likes = h3("Likes: " + str(post.likes))
             comments = h3("Comments: " + str(post.comments))
-            image = img(src=post.url, alt = "a post picture", crossorigin="anonymous")
+            image = img(src=instagram_post_cache_location(post))
             body.appendChild(header)
             body.appendChild(likes)
             body.appendChild(comments)
@@ -52,6 +58,37 @@ class TopPostsCustomerProfileHtmlGenerator(HtmlGenerator):
 
         return body
 
+
+#Ref: https://www.geeksforgeeks.org/download-instagram-profile-pic-using-python/
+def download_instagram_image(post: InstagramPost):
+    filename = instagram_post_cache_location(post)
+    file_exists = os.path.isfile(filename)
+ 
+    if not file_exists:
+        with open(filename, 'wb') as handle:
+            response = requests.get(post.url, stream=True)
+            if not response.ok:
+                print(response)
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+
+
+CACHE_LOCATION = os.path.join(os.getcwd(),".cache/content")
+
+def _create_cache_folder():
+    path = CACHE_LOCATION
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def instagram_post_cache_location(post: InstagramPost):
+    return CACHE_LOCATION + "/" + instagram_post_to_filename(post)
+
+
+def instagram_post_to_filename(post: InstagramPost):
+    return post.url.split("?")[0].replace("https://", "").split("/")[-1]
 
 
 
